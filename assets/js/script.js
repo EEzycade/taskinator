23355545c5a1056f8c916909c18f613747041fd9
Cade Ellsworth <caedmd2100@gmail.com>
2021-04-03 12:32:19 -0700

    made the submit button work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createNewTask = function() {
+var createNewTask = function(event) {
+    event.preventDefault();
     var tasksListEl = document.createElement("li");
     tasksListEl.className = "task-item";
     tasksListEl.textContent = "This is a new task";
     tasksToDoEl.appendChild(tasksListEl);
 }
 
-buttonEl.addEventListener("click", createNewTask);
+formEl.addEventListener("submit", createNewTask);
