fac1cfed753ee99604e8c458013696842f311cd2
Cade Ellsworth <caedmd2100@gmail.com>
2021-04-05 14:28:22 -0700

    create new task with single function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createNewTask = function(event) {
+var createTaskHandler = function (event) {
     event.preventDefault();
-    var tasksListEl = document.createElement("li");
-    tasksListEl.className = "task-item";
-    tasksListEl.textContent = "This is a new task";
-    tasksToDoEl.appendChild(tasksListEl);
-}
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-formEl.addEventListener("submit", createNewTask);
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info"
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
